571c1e923aa102ae8eb37b2f8c16d4630c23c25c
정민호 <alsgh846@gmail.com>
2025-12-04 18:08:08 +0900

    move properties

--- src/main/java/minhobot/coincalculator/config/FirebaseConfig.java
+++ src/main/java/minhobot/coincalculator/config/FirebaseConfig.java
@@ -4,20 +4,24 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
 import java.io.InputStream;
 
 @Slf4j
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.key-path}")
+    private String firebaseKeyPath;
+
     @PostConstruct
     public void init() {
         try {
-            InputStream serviceAccount =
-                    getClass().getClassLoader().getResourceAsStream("firebase-service-key.json");
+            InputStream serviceAccount = new FileInputStream(firebaseKeyPath);
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
